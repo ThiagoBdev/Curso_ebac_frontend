3cbbc5c246a7685adc8429a97311fa7e101c5ea1
Thiago Batista <140573879+ThiagoBdev@users.noreply.github.com>
2025-01-30 17:28:59 -0300

    Add files via upload

---
+++ Exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicacao(Numero1: number, Numero2:number): number {
+    return Numero1 * Numero2;
+}
+
+function saudacao(Nome: string): string{
+    return "olá" + Nome;
+}
+
+console.log(saudacao("Thiago"))
+console.log(multiplicacao(5, 5))
